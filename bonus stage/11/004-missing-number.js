8812492656a3435df3395e5931c4d5ae0937dff5
Hesa Muhammad <hesamilan@gmail.com>
2019-10-21 21:22:21 +0700

    goodluck

--- bonus stage/11/004-missing-number.js
+++ bonus stage/11/004-missing-number.js
@@ -36,3 +36,46 @@ console.log(missingNum([[ 2,' '], [' ',5]])) // [ 3, 4 ]
 console.log(missingNum([[11,' ', 13], [17,' ', 19], [' ',16,' ']])) // [ 12, 14, 15, 18 ]
 console.log(missingNum([[3,' ', 5, 15], [1,' ', 7, 13], [9,' ',' ', 12], [' ', 16,' ',' ']])) // [ 2, 4, 6, 8 ]
 console.log(missingNum([])) // []
+
+
+// function sort(data) {
+//   for (var i = 0; i < data.length; i++) {
+//     for (var j = 0; j < data.length; j++) {
+//       if (data[j] > data[j + 1]) {
+//         var temp = data[j]
+//         data[j] = data[j + 1]
+//         data[j + 1] = temp
+//       }
+//     }
+//   }
+//   return data
+// }
+
+// function missingNum(arr) {
+//   // Your code here
+//   var checkNum = []
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       // console.log(arr[i][j])
+//       if (arr[i][j] !== " ") {
+//         checkNum.push(arr[i][j])
+//       }
+//     }
+//   }
+//   var sorting = sort(checkNum)
+//   var start = checkNum[0]
+//   var end = checkNum[checkNum.length - 1]
+//   var result = []
+//   for (let i = start; i <= end; i++) {
+//     var flag = true
+//     for (let j = 0; j <= sorting.length; j++) {
+//       if (i === sorting[j]) {
+//         flag = false
+//       }
+//     }
+//     if (flag === true) {
+//       result.push(i)
+//     }
+//   }
+//   return result
+// }
